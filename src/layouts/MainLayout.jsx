01eb8d4252a8b38fafcd29f474cea10d87c3dfb6
Bichoo <bichoyatef0@gmail.com>
2025-01-31 00:35:54 +0200

    dd

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,19 +4,19 @@ import Footer from '@/components/Layout/Footer';
 
 export default function Layout() {
     return (
-        <div className='flex flex-col h-screen bg-gray-900 overflow-hidden'>
+        <div className="flex flex-col h-screen bg-gray-900 overflow-hidden">
             {/* Fixed NavBar at the top */}
-            <header className='fixed top-0 left-0 right-0 z-10 border-b border-gray-800'>
+            <header className="fixed top-0 left-0 right-0 z-10 border-b border-gray-800">
                 <NavBar />
             </header>
 
             {/* Main content area with scrollable content */}
-            <main className='flex-grow mt-[50px] mb-[50px] overflow-y-auto custom-scrollbar'>
+            <main className="flex-grow mt-[50px] mb-[50px] overflow-y-auto custom-scrollbar">
                 <Outlet />
             </main>
 
             {/* Fixed Footer at the bottom */}
-            <footer className='fixed bottom-0 left-0 right-0 z-10 border-t border-gray-800'>
+            <footer className="fixed bottom-0 left-0 right-0 z-10 border-t border-gray-800">
                 <Footer />
             </footer>
         </div>
